9973a3da6ed88f0802a5c8058efb86f7c82bc66f
Song Fang Yi <songfangyi1210@gmail.com>
2025-03-16 12:39:24 +0000

    LOL we need a websocket to pass data... lfg :)

--- api/app/services/llms/cli.py
+++ api/app/services/llms/cli.py
@@ -2,17 +2,20 @@ import click
 from crewai import LLM
 
 from app.services.llms.roadSeek import RoadSeek
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 
 
 @click.command()
-@click.option(
-    "--data", prompt="Input data", help="The data to be processed by the agent."
-)
+@click.option("--data", prompt="Input data", help="The data to be processed by the agent.")
 def main(data):
     road_seek = RoadSeek(
         llm=LLM(
             model="deepseek/deepseek-chat",
-            api_key="",
+            api_key=DEEPSEEK_API_KEY,
         )
     )
     road_seek.crew().kickoff(inputs={"data": data})
